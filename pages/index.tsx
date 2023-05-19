502c6d22d74f2ae5481217e178f32d1396657b35
Flavio78 <53538168+Flavio78@users.noreply.github.com>
2023-05-19 22:38:48 +0200

    Tutorial 13

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,17 @@
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 const Home = () => {
+  const router = useRouter();
+  const orderHandler = () => {
+    router.push('/product');
+  };
   return (
     <div>
       <h1>Home Page</h1>
       <Link href="/blog">Blog</Link>
       <Link href="/product">Products</Link>
+      <button onClick={orderHandler}>Place Order</button>
     </div>
   );
 };
--- pages/product/index.tsx
+++ pages/product/index.tsx
@@ -4,9 +4,15 @@ const ProductList = () => {
   return (
     <div>
       <Link href="/">Home</Link>
-      <Link href="/product/1">Product 1</Link>
-      <Link href="/product/2">Product 2</Link>
-      <Link href="/product/3">Product 3</Link>
+      <h2>
+        <Link href="/product/1">Product 1</Link>
+      </h2>
+      <h2>
+        <Link href="/product/2">Product 2</Link>
+      </h2>
+      <h2>
+        <Link href="/product/3">Product 3</Link>
+      </h2>
     </div>
   );
 };
