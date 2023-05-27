85fa8faeee12956f916b40688bef7421de8b36fb
Flavio78 <53538168+Flavio78@users.noreply.github.com>
2023-05-27 16:55:49 +0200

    Tutorial 39: end

--- pages/events.tsx
+++ pages/events.tsx
@@ -13,10 +13,13 @@ const EventList = ({ eventsList }: EventListProps) => {
   const [events, setEvents] = useState(eventsList);
   const router = useRouter();
   const fetchSportEvents = async () => {
-    const response = await fetch(
-      'http://localhost:4000/events?category=sports'
-    );
-    const data = (await response.json()) as Events;
+    let data: Events = [];
+    try {
+      const response = await fetch(
+        'http://localhost:4000/events?category=sports'
+      );
+      data = (await response.json()) as Events;
+    } catch (error) {}
     setEvents(data);
     router.push('/events?category=sports', undefined, { shallow: true });
   };
@@ -25,6 +28,7 @@ const EventList = ({ eventsList }: EventListProps) => {
       <Link href="/">Back to home</Link>
       <button onClick={fetchSportEvents}>Sport events</button>
       <h1>List of events</h1>
+      {!events.length && <div>No events</div>}
       {events.map((event) => (
         <div key={event.id}>
           <h2>
@@ -47,11 +51,17 @@ export const getServerSideProps: GetServerSideProps<
 > = async (context) => {
   const { category } = context.query;
   const queryString = category ? `category=sports` : '';
-  const response = await fetch(`http://localhost:4000/events?${queryString}`);
-  const eventsList = (await response.json()) as Events;
-  return {
-    props: { eventsList },
-  };
+  try {
+    const response = await fetch(`http://localhost:4000/events?${queryString}`);
+    const eventsList = (await response.json()) as Events;
+    return {
+      props: { eventsList },
+    };
+  } catch (error) {
+    return {
+      props: { eventsList: [] },
+    };
+  }
 };
 
 export default EventList;
