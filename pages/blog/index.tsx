750cc00b585eb2c8961d5504e792ac920c1b1069
Flavio78 <53538168+Flavio78@users.noreply.github.com>
2023-06-03 21:05:21 +0200

    Some route

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,5 +1,13 @@
+import Link from 'next/link';
+
 const Blog = () => {
-  return <h1>Blog Page</h1>;
+  return (
+    <div>
+      <Link href="/">Back to Home</Link>
+      <h1>Blog Page</h1>
+      <Link href="/blog/1">Go to blog 1</Link>
+    </div>
+  );
 };
 
 export default Blog;
