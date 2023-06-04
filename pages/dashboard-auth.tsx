d12ab94c34682e6c23b9502e60a8b8fc081938a1
Flavio78 <53538168+Flavio78@users.noreply.github.com>
2023-06-04 20:43:51 +0200

    Tutorial 72: end

--- pages/dashboard-auth.tsx
+++ pages/dashboard-auth.tsx
@@ -1,4 +1,24 @@
+import { getSession, signIn } from 'next-auth/react';
+import { useEffect, useState } from 'react';
+
 const DashboardAuth = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const securePage = async () => {
+      const session = await getSession();
+      if (!session) {
+        signIn('github');
+      } else {
+        setLoading(false);
+      }
+    };
+    securePage();
+    return () => {};
+  }, []);
+  if (loading) {
+    return <h2>Loading ...</h2>;
+  }
   return <h1>Dashboard Auth Page</h1>;
 };
 
