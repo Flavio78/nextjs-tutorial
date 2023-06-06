2d3ba914108082d4f34ca9ea4a65707c1bd21d49
Flavio78 <53538168+Flavio78@users.noreply.github.com>
2023-06-06 22:34:07 +0200

    Tutorial 76: end

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,11 +1,20 @@
-import NextAuth from 'next-auth/next';
+import clientPromise from '@/lib/mongodb';
+import { MongoDBAdapter } from '@next-auth/mongodb-adapter';
+import NextAuth, { NextAuthOptions } from 'next-auth';
 import GitHubProvider from 'next-auth/providers/github';
 
-export default NextAuth({
+const options: NextAuthOptions = {
   providers: [
     GitHubProvider({
       clientId: process.env.GITHUB_ID!,
       clientSecret: process.env.GITHUB_SECRET!,
     }),
   ],
-});
+  adapter: MongoDBAdapter(clientPromise),
+  session: {
+    strategy: 'jwt',
+  },
+  secret: 'aRlQxUr_12wH!g54?hq4t1z',
+};
+
+export default NextAuth(options);
